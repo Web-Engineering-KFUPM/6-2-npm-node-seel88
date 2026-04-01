d68f6342f74304a9b0d09f443aad74e6ed32b510
seel88 <s202270080@kfupm.edu.sa>
2026-04-01 08:42:07 +0300

    Completed registration form and lodash parseNumbers fix

--- utils/parser.js
+++ utils/parser.js
@@ -1,14 +1,14 @@
 import _ from "lodash";
 
 export function parseNumbers(input) {
-
+  // 1. Map the input strings to Numbers
   const numbers = _.map(input, (str) => Number(str));
-  return _.filter(numbers, (n) => !isNaN(n));
+
+  // 2. Use _.compact to remove all falsy values (including NaN)
+  return _.compact(numbers);
 }
 
 export function isValidOperation(operation) {
-
   const validOps = ["add", "subtract", "multiply", "divide"];
-
   return _.includes(validOps, operation);
 }
