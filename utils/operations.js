7ee1a59b2b98119e2b281a7bdad3f2d1cfb899b5
seel88 <s202270080@kfupm.edu.sa>
2026-04-01 08:16:06 +0300

    Completed TODO 1: Math operations

---
+++ utils/operations.js
@@ -0,0 +1,6 @@
+export function add(a, b) { return a + b; }
+export function subtract(a, b) { return a - b; }
+export function multiply(a, b) { return a * b; }
+export function divide(a, b) { 
+    return b === 0 ? "Error: Division by zero" : a / b; 
+}
